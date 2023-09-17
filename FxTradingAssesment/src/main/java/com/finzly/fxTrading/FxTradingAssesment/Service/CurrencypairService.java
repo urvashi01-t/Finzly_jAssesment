9b76a5bef477b16b5686ec79cff68d4e3df43ebe
urvashi <74699524+urvashi01-t@users.noreply.github.com>
2023-09-17 19:44:16 +0530

    CurrencypairService.java

--- FxTradingAssesment/src/main/java/com/finzly/fxTrading/FxTradingAssesment/Service/CurrencypairService.java
+++ FxTradingAssesment/src/main/java/com/finzly/fxTrading/FxTradingAssesment/Service/CurrencypairService.java
@@ -2,19 +2,32 @@ package com.finzly.fxTrading.FxTradingAssesment.Service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.finzly.fxTrading.FxTradingAssesment.Dao.CurrencypairDao;
+import com.finzly.fxTrading.FxTradingAssesment.Exception.CurrencypairNotAvailableException;
 import com.finzly.fxTrading.FxTradingAssesment.Model.Currencypair;
 
 @Service
 public class CurrencypairService {
+	  // Create a logger for this class
+	Logger logger = LoggerFactory.getLogger(CurrencypairService.class);
+
 	@Autowired
 CurrencypairDao dao;
-
+	// Retrieve a list of currency pairs from the database
 public List<Currencypair> getCurrencyPair() {
-	System.out.println("it works");
-	return dao.getCurrencyPair();
+	logger.info("Fetching all currency pair from data base (CurrencypairService)");
+    // Check if any currency pairs are available; if not, throw an exception
+
+	if( dao.getCurrencyPair().isEmpty() ) {
+		throw new CurrencypairNotAvailableException("Requested Currency Pair is not available");
+	}
+	else
+		return  dao.getCurrencyPair();
+	
 }
 }
