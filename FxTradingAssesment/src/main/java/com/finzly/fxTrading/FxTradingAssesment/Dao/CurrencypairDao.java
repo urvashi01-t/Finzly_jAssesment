312a567c8deb4dc0e4c0e84323ecc1ec189622a8
urvashi <74699524+urvashi01-t@users.noreply.github.com>
2023-09-17 19:43:34 +0530

    CurrencypairDao.java

--- FxTradingAssesment/src/main/java/com/finzly/fxTrading/FxTradingAssesment/Dao/CurrencypairDao.java
+++ FxTradingAssesment/src/main/java/com/finzly/fxTrading/FxTradingAssesment/Dao/CurrencypairDao.java
@@ -1,33 +1,36 @@
-package com.finzly.fxTrading.FxTradingAssesment.Service;
+package com.finzly.fxTrading.FxTradingAssesment.Dao;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.finzly.fxTrading.FxTradingAssesment.Dao.CurrencypairDao;
-import com.finzly.fxTrading.FxTradingAssesment.Exception.CurrencypairNotAvailableException;
 import com.finzly.fxTrading.FxTradingAssesment.Model.Currencypair;
+import com.finzly.fxTrading.FxTradingAssesment.controller.CurrencypairController;
 
-@Service
-public class CurrencypairService {
-	  // Create a logger for this class
-	Logger logger = LoggerFactory.getLogger(CurrencypairService.class);
-
+@Repository
+public class CurrencypairDao {
+	// Create a logger for this class
+	Logger logger = LoggerFactory.getLogger(CurrencypairDao.class);
 	@Autowired
-CurrencypairDao dao;
-	// Retrieve a list of currency pairs from the database
-public List<Currencypair> getCurrencyPair() {
-	logger.info("Fetching all currency pair from data base (CurrencypairService)");
-    // Check if any currency pairs are available; if not, throw an exception
+	SessionFactory factory;
+	 // Retrieve a list of currency pairs from the database
+	public List<Currencypair> getCurrencyPair() {
+		logger.info("Fetching all currency pair from data base (CurrencypairDao)");
+        // Open a session to interact with the database
 
-	if( dao.getCurrencyPair().isEmpty() ) {
-		throw new CurrencypairNotAvailableException("Requested Currency Pair is not available");
+		Session session = factory.openSession();
+		Criteria criteria = session.createCriteria(Currencypair.class);
+        // Return the list of currency pairs
+
+		return criteria.list();
+
 	}
-	else
-		return  dao.getCurrencyPair();
-	
-}
+
 }
